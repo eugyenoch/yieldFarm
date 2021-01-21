b8da30c764b223f6378645ef84e8306fe11d456a
Eugy Enoch <38564978+eugyenoch@users.noreply.github.com>
2021-01-21 11:51:52 +0100

    Update 3_deploy_tokenfarm.js

--- migrations/3_deploy_tokenfarm.js
+++ migrations/3_deploy_tokenfarm.js
@@ -6,30 +6,31 @@ module.exports = async function(deployer, network, accounts) {
   const dappToken = await DappToken.deployed();
   await deployer.deploy(TokenFarm, dappToken.address);
   const tokenFarm = await TokenFarm.deployed();
-  await dappToken.transfer(tokenFarm.address, "1000000000000000000000000");
+  await dappToken.transfer(tokenFarm.address, "10000000000000000000000");
 
-  if (network.startsWith("kovan") || network.startsWith("live")) {
+  if (network.startsWith("kovan") || network.startsWith("live") || network.startsWith("Binance")) {
     // LINK Token address
     await tokenFarm.addAllowedTokens(
-      "0xa36085F69e2889c224210F603D836748e7dC0088"
+      "0x9EA8d6272B724AfB813A9E7C4eaF07C7aF4FE996"
     );
     await tokenFarm.setPriceFeedContract(
-      "0xa36085F69e2889c224210F603D836748e7dC0088",
-      "0x3Af8C569ab77af5230596Acf0E8c2F9351d24C38"
+      "0x9EA8d6272B724AfB813A9E7C4eaF07C7aF4FE996",
+      "0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE"
     );
     // FAU Token address. Pretending FAU is DAI
     await tokenFarm.addAllowedTokens(
-      "0xFab46E002BbF0b4509813474841E0716E6730136"
+      "0xe93a027074c23f50253a9fe4BB3608902B3F5cCB"
     );
     await tokenFarm.setPriceFeedContract(
-      "0xFab46E002BbF0b4509813474841E0716E6730136",
-      "0x777A68032a88E5A84678A77Af2CD65A7b3c0775a"
+      "0xe93a027074c23f50253a9fe4BB3608902B3F5cCB",
+      "0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE"
     );
     // DAPP Token Address - also dai
     await tokenFarm.addAllowedTokens(dappToken.address);
     await tokenFarm.setPriceFeedContract(
-      dappToken.address,
-      "0x777A68032a88E5A84678A77Af2CD65A7b3c0775a"
+      //dappToken.address,
+       "0xe93a027074c23f50253a9fe4BB3608902B3F5cCB",
+      "0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE"
     );
   }
 };
