360b18c25c1bf7d55164e12296168b1f111196d7
Eugy Enoch <38564978+eugyenoch@users.noreply.github.com>
2021-01-21 11:24:24 +0100

    Update DappToken.sol

--- src/contracts/DappToken.sol
+++ src/contracts/DappToken.sol
@@ -1,15 +1,3 @@
-/**
-pragma solidity ^0.6.0;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract DappToken is ERC20 {
-    constructor() public ERC20("DApp Token", "DAPP") {
-        _mint(msg.sender, 1000000000000000000000000);
-    }
-}
-**/
-
 pragma solidity 0.5.16;
 
 interface IBEP20 {
