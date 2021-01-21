53272cbca390b74e8ddbb2519b7678eb06e51f24
Eugy Enoch <38564978+eugyenoch@users.noreply.github.com>
2021-01-21 12:09:34 +0100

    Update get-stats.js

--- scripts/get-stats.js
+++ scripts/get-stats.js
@@ -3,7 +3,7 @@ const TokenFarm = artifacts.require("TokenFarm");
 module.exports = async function(callback) {
   let tokenFarm = await TokenFarm.deployed();
   let totalValue = await tokenFarm.getUserTotalValue(
-    "0x643315C9Be056cDEA171F4e7b2222a4ddaB9F88D"
+    "0x9ea8d6272b724afb813a9e7c4eaf07c7af4fe996"//"0x643315C9Be056cDEA171F4e7b2222a4ddaB9F88D"
   );
 
   console.log(totalValue.toString());
