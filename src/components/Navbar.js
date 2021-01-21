d472a6d8dad5f1733dc9b7eab819f7f506a54aa8
Eugy Enoch <38564978+eugyenoch@users.noreply.github.com>
2021-01-21 12:48:24 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ class Navbar extends Component {
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="https://alphachain.io"
+          href="https://#0"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -18,7 +18,7 @@ class Navbar extends Component {
             className="d-inline-block align-top"
             alt=""
           />
-          &nbsp; DApp Token Farm
+          &nbsp; Yield Token Farm
         </a>
 
         <ul className="navbar-nav px-3">
